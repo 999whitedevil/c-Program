f9eb890224717b53a2b57baa534ef02f7c835a19
aditya kumar sahu <132786741+999whitedevil@users.noreply.github.com>
2024-08-07 01:59:44 +0530

    Update and rename main.c (2) to finding-highest-numbe- in-2-rows-using-loop-statement.c
    finding-highest-numbe- in-2-rows-using-loop-statement

--- finding-highest-numbe- in-2-rows-using-loop-statement.c
+++ finding-highest-numbe- in-2-rows-using-loop-statement.c
@@ -1,9 +1,5 @@
 /******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
+//finding-highest-numbe- in-2-rows-using-loop-statement
 *******************************************************************************/
 
 #/* Online C Compiler and Editor */
